4957d39c35f00aa42c23103c380552cfb113c2aa
Sidney-Land <136678741+Sidney-Land@users.noreply.github.com>
2025-04-21 12:44:56 -0400

    Added Level Exit Door and Loading Functionality
    + Added the Level Exit Door prefab
    
    + Added Level Exit.cs to facilitate loading the next level and storing best times. Currently it loads the next level in build settings or the first level in build settings if there is no "next level".
    
    ** NOTE Level Exit.cs still needs best time storing functionality!!
    
    + Made a basic design of Wall-Running Tutorial, now I just need a sign to tell how wall-running works, walls, and the kill floor.

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    // Current Scene index (preferably in build settings)
+    static int thisSceneIndex;
+    static int builtScenesCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get current scene index
+        thisSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Get built scenes count
+        builtScenesCount = SceneManager.sceneCountInBuildSettings;
+    }
+
+    // Upon reaching Level Exit Door, load the next level and store the current best time (if better)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (thisSceneIndex + 1 < builtScenesCount)
+        {
+            SceneManager.LoadScene(thisSceneIndex + 1);
+        }
+        // If next Scene is not in build settings, load Main Menu
+        else
+        {
+            Debug.Log("Loading Main Menu");
+            SceneManager.LoadScene(0);
+        }
+    }
+
+
+}
